1036599aa4ac0c417e21f4443c1129b5dd891a32
Turgunov Sunnatilla Shuxrat o'g'li <sunnatturgunovno1@gmail.com>
2026-03-04 14:56:59 +0500

    fix: add DEFAULT_AUTO_FIELD and test_settings for SQLite testing

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
