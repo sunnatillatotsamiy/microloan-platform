6dd4c188f558700f307c82e45625608dd5f5e567
Turgunov Sunnatilla Shuxrat o'g'li <sunnatturgunovno1@gmail.com>
2026-03-05 10:05:04 +0500

    fixed users urls.py import error

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import RegisterView, UserListView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('', UserListView.as_view(), name='user-list'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
